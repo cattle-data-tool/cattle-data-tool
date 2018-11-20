e179470a41a2c178aa5ce610212f2f5b5933861f
Ivica Matić <ivica1112@net.hr>
2018-11-20 18:55:59 +0000

    Sorry guys, I made shit

--- Data Part/datamain.py
+++
@@ -1,7 +0,0 @@
-from data import Data
-
-data = Data()
-
-data.add_csv("DATA_01_05_Cow_407.csv") #import data from csv,can also specify location on computer,duplicate files rejected
-
-print(data.getAccel(407))
