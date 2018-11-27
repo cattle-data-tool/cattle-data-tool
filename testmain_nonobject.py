27febb964aa25011531ac48b22755951c5f03536
Ivica Matić <ivica1112@net.hr>
2018-11-27 15:20:02 +0000

    Rohit and IcE Rules

---
+++ testmain_nonobject.py
@@ -0,0 +1,9 @@
+
+from plotter import plotter_math
+from data import CsvDataBase
+data = CsvDataBase()
+data.add_csv("DATA_01_05_Cow_42.csv")
+
+accels_x = data.getAccel(42,'acc_x_g')
+accels_y = data.getAccel(42,'acc_y_g')
+print(plotter_math(accels_x,accels_y))
--- testmain_object.py
+++ testmain_object.py
@@ -1,10 +1,10 @@
 
-from plotter import plotter_math
-
+from plotter_object import Plotter
+plotter = Plotter()
 from data import CsvDataBase
 data = CsvDataBase()
 data.add_csv("DATA_01_05_Cow_42.csv")
 
 accels_x = data.getAccel(42,'acc_x_g')
 accels_y = data.getAccel(42,'acc_y_g')
-print(plotter_math(accels_x,accels_y))
+print(plotter.plotter_math(accels_x,accels_y))
