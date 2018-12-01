cb05d9d58d4695f41e27efdab6373e41bd352591
PetarPeychev <petarpeychev98@gmail.com>
2018-12-01 22:06:20 +0000

    rolled back random starting positions

--- plotter.py
+++ plotter.py
@@ -3,7 +3,7 @@ from statistics import mean
 
 class Plotter:
 
-   
+
 
     def __init__(self,data):
         self.data = data
@@ -11,33 +11,33 @@ class Plotter:
         self.dict = []
         self.u_x = 0
         self.u_y = 0
-    
-    
+
+
 
     def position(self,v_x, v_y):
         a = (v_x/1000, v_y/1000)
         self.dict.append(a)
 
     def plotter_math(self,id):
-        
+
         di_x = self.data.getAccel(id,'acc_x_g')
         di_y = self.data.getAccel(id,'acc_y_g')
         self.t = 1
         self.dict = []
         self.u_x = 0
         self.u_y = 0
-        i = 0 
+        i = 0
         t = 1
         n = (len(di_x) - 1)
-        v_x = random.randint(-5000, 5000)
-        v_y = random.randint(-5000, 5000)
+        v_x = 0
+        v_y = 0
         while i <= n:
-        
+
             a_x = di_x[i] #current accels from csv file
             a_y = di_y[i]
-        
+
            #print("u_x previous value is ",self.u_x)
-            s_x = (self.u_x*t)+(0.5*(a_x*t)) # we get displacemnt s 
+            s_x = (self.u_x*t)+(0.5*(a_x*t)) # we get displacemnt s
             #print("s_x calculated value",s_x)
             s_y = (self.u_y*t)+(0.5*(a_y*t))
 
@@ -84,7 +84,7 @@ class Plotter:
             i = 0
             _cordyavg = []
             _cordyavgcache = []
-            
+
             for n in range(0,len(coords_y)):
                 _cordyavgcache.append(coords_y[n]) #append current coord to cache
                 i += 1
@@ -93,14 +93,5 @@ class Plotter:
                     i = 0
                     _cordyavg.append(mean(_cordyavgcache)) #append avg of cache to variable _cordxavg
                     _cordyavgcache = [] #clear cache
-        
+
             return(_cordxavg,_cordyavg)
-
-
-
-
-
-    
-
-
-
