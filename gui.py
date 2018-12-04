fe08b893045e878b0523029b5884d5bfeaeff99d
PetarPeychev <petarpeychev98@gmail.com>
2018-12-04 01:01:29 +0000

    added table class and moved menu to start page

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 from matplotlib.figure import Figure
 
 import tkinter as tk
+import tkinter.font as tkFont
 from tkinter import ttk
 
 from collections import namedtuple
@@ -67,32 +68,6 @@ class Gui(tk.Tk):
         container.grid_rowconfigure(0, weight = 1)
         container.grid_columnconfigure(0, weight = 1)
 
-        menubar = tk.Menu(container)
-
-        filemenu = tk.Menu(menubar, tearoff = 0)
-        filemenu.add_command(label = "Save Workspace", command = lambda: popupmsg("Not Implemented Yet"))
-        filemenu.add_command(label = "Load Workspace", command = lambda: popupmsg("Not Implemented Yet"))
-        filemenu.add_separator()
-        filemenu.add_command(label = "Exit", command = self.destroy)
-        menubar.add_cascade(label = "File", menu = filemenu)
-
-        datamenu = tk.Menu(menubar, tearoff = 0)
-        datamenu.add_command(label = "Import CSV", command = lambda: popupmsg("Not Implemented Yet"))
-        datamenu.add_command(label = "Remove Selected", command = lambda: popupmsg("Not Implemented Yet"))
-        datamenu.add_command(label = "Plot Selected", command = lambda: popupmsg("Not Implemented Yet"))
-        menubar.add_cascade(label = "Data", menu = datamenu)
-
-        selectmenu = tk.Menu(menubar, tearoff = 0)
-        selectmenu.add_command(label = "Select All", command = lambda: popupmsg("Not Implemented Yet"))
-        selectmenu.add_command(label = "Select None", command = lambda: popupmsg("Not Implemented Yet"))
-        menubar.add_cascade(label = "Select", menu = selectmenu)
-
-        helpmenu = tk.Menu(menubar, tearoff = 0)
-        helpmenu.add_command(label = "Open Documentation", command = lambda: popupmsg("Not Implemented Yet"))
-        menubar.add_cascade(label = "Help", menu = helpmenu)
-
-        tk.Tk.config(self, menu = menubar)
-
         self.frames = {}
 
         for f in (StartPage,):
@@ -109,8 +84,35 @@ class Gui(tk.Tk):
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "Hello, Gui!", font = LARGE_FONT)
-        label.pack(pady = 10, padx = 10)
+
+        menubar = tk.Menu(controller)
+
+        filemenu = tk.Menu(menubar, tearoff = 0)
+        filemenu.add_command(label = "Save Workspace", command = lambda: popupmsg("Not Implemented Yet"))
+        filemenu.add_command(label = "Load Workspace", command = lambda: popupmsg("Not Implemented Yet"))
+        filemenu.add_separator()
+        filemenu.add_command(label = "Exit", command = self.destroy)
+        menubar.add_cascade(label = "File", menu = filemenu)
+
+        datamenu = tk.Menu(menubar, tearoff = 0)
+        datamenu.add_command(label = "Import CSV", command = lambda: popupmsg("Not Implemented Yet"))
+        datamenu.add_command(label = "Remove Selected", command = lambda: popupmsg("Not Implemented Yet"))
+        datamenu.add_command(label = "Plot Selected", command = lambda: display_graph(curve_list))
+        menubar.add_cascade(label = "Data", menu = datamenu)
+
+        selectmenu = tk.Menu(menubar, tearoff = 0)
+        selectmenu.add_command(label = "Select All", command = lambda: popupmsg("Not Implemented Yet"))
+        selectmenu.add_command(label = "Select None", command = lambda: popupmsg("Not Implemented Yet"))
+        menubar.add_cascade(label = "Select", menu = selectmenu)
+
+        helpmenu = tk.Menu(menubar, tearoff = 0)
+        helpmenu.add_command(label = "Open Documentation", command = lambda: popupmsg("Not Implemented Yet"))
+        menubar.add_cascade(label = "Help", menu = helpmenu)
+
+        tk.Tk.config(controller, menu = menubar)
+
+        # label = tk.Label(self, text = "Hello, Gui!", font = LARGE_FONT)
+        # label.pack(pady = 10, padx = 10)
 
         # TODO: fix windows file paths
         controller.data.add_csv("DATA_01_05_Cow_42.csv")
@@ -131,6 +133,74 @@ class StartPage(tk.Frame):
             coords_x, coords_y = controller.plotter.plot(id)
             curve_list.append(Curve(coords_x, coords_y))
 
-        button1 = ttk.Button(self, text = "Show Graph",
-                                    command = lambda: display_graph(curve_list))
-        button1.pack()
+        # button1 = ttk.Button(self, text = "Show Graph",
+        #                             command = lambda: display_graph(curve_list))
+        # button1.pack()
+
+        header = ['car', 'repair']
+        data = [
+        ('Hyundai', 'brakes') ,
+        ('Honda', 'light') ,
+        ('Lexus', 'battery') ,
+        ('Benz', 'wiper') ,
+        ('Ford', 'tire') ,
+        ('Chevy', 'air') ,
+        ('Chrysler', 'piston') ,
+        ('Toyota', 'brake pedal') ,
+        ('BMW', 'seat')
+        ]
+
+        table = Table(header, data)
+        table.container.pack(fill = tk.BOTH, expand = True)
+
+class Table:
+    def __init__(self, header, data):
+        self.header = header
+        self.data = data
+        self.tree = None
+        self.setup_widgets()
+        self.build_tree()
+
+    def setup_widgets(self):
+        self.container = ttk.Frame()
+
+        self.tree = ttk.Treeview(columns = self.header, show = "headings", height = 50)
+        vertical_scrollbar = ttk.Scrollbar(orient = "vertical",
+                                       command = self.tree.yview)
+        horizontal_scrollbar = ttk.Scrollbar(orient = "horizontal",
+                                             command = self.tree.xview)
+
+        self.tree.configure(yscrollcommand = vertical_scrollbar.set,
+                            xscrollcommand = horizontal_scrollbar.set)
+
+        self.tree.grid(column = 0, row = 0, sticky = 'nsew', in_ = self.container)
+
+        vertical_scrollbar.grid(column = 1, row = 0, sticky = 'ns', in_ = self.container)
+        horizontal_scrollbar.grid(column = 0, row = 1, sticky = 'ew', in_ = self.container)
+
+        self.container.grid_columnconfigure(0, weight = 1)
+        self.container.grid_rowconfigure(0, weight = 1)
+
+    def build_tree(self):
+        for col in self.header:
+            self.tree.heading(col, text = col.title(),
+                              command = lambda c = col: self.sortby(self.tree, c, 0))
+
+            self.tree.column(col,
+                             width = tkFont.Font().measure(col.title()))
+
+        for item in self.data:
+            self.tree.insert('', 'end', values = item)
+
+            for x, val in enumerate(item):
+                col_w = tkFont.Font().measure(val)
+                if self.tree.column(self.header[x], width = None) < col_w:
+                    self.tree.column(self.header[x], width = col_w)
+
+    def sortby(self, tree, col, descending):
+        data = [(tree.set(child, col), child) for child in tree.get_children('')]
+        data.sort(reverse = descending)
+        for x, item in enumerate(data):
+            tree.move(item[1], '', x)
+        tree.heading(col, command = lambda col = col: self.sortby(
+            tree, col, int(not descending)))
